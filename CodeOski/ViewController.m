a360f60775c56ee1bd4252d501339c17571d11cc
Sony Theakanath <stheakanath@gmail.com>
2014-10-04 16:15:39 -0700

    Code is pretty now!

--- CodeOski/ViewController.m
+++ CodeOski/ViewController.m
@@ -72,111 +72,47 @@ UIDatePicker *datePicker;
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section { return _items.count; }
 
 
+#pragma mark - User Clickers (IBActions)
 
-
-
-
-
-
-- (IBAction)switchChanged: (id)sender {
+- (IBAction)switchChanged:(id)sender {
     UISwitch *switchInCell = (UISwitch *)sender;
     UITableViewCell * cell = (UITableViewCell*) switchInCell.superview;
     NSIndexPath * indexpath = [alarms indexPathForCell:cell];
     
     NSString *boolean = @"1";
-    if (!switchInCell.on) {
-        boolean = @"0";
-    }
+    if (!switchInCell.on) boolean = @"0";
     
+    //Saving Items
     NSString* dataPath = [[NSString alloc] initWithString:[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"data.plist"]];
     NSMutableArray *data = [[NSMutableArray alloc] init];
-    if([[[NSFileManager alloc] init] fileExistsAtPath:dataPath])
-        data = [NSKeyedUnarchiver unarchiveObjectWithFile:dataPath];
-    NSLog(@"%@", boolean);
-    NSLog(@"%@", data[indexpath.row][2]);
+    if([[[NSFileManager alloc] init] fileExistsAtPath:dataPath]) data = [NSKeyedUnarchiver unarchiveObjectWithFile:dataPath];
     NSArray *alarm = @[@"Alarm Name1", data[indexpath.row][1], boolean];
     [data replaceObjectAtIndex:indexpath.row withObject:alarm];
-
-    //[[data objectAtIndex:] objectAtIndex:2] = boolean;
-  //  data[indexpath.row][2] = boolean;
     _items = data;
     [[NSKeyedArchiver archivedDataWithRootObject:data] writeToFile:dataPath atomically:YES];
-
-
-    [alarms reloadData];
-    
-}
-
-
-
-
-
-
-- (void)viewDidLoad {
-    alarms = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width, [[UIScreen mainScreen] bounds].size.height)];
-    alarms.delegate = self;
-    alarms.dataSource = self;
-    [alarms setBackgroundColor:[UIColor blackColor]];
-    // [alarms setSeparatorStyle:UITableViewCellSeparatorStyleNone];
-    [self.view addSubview:alarms];
-    [self setNeedsStatusBarAppearanceUpdate];
-    UIBarButtonItem *addAlert = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addalarm:)];
-    [self.navigationItem setRightBarButtonItem:addAlert];
-    [self.navigationItem setTitle:@"CodeOski"];
-    [self loadOldAlarms];
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void) loadOldAlarms {
-    NSString* dataPath = [[NSString alloc] initWithString:[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"data.plist"]];
-    NSMutableArray *data = [[NSMutableArray alloc] init];
-    if([[[NSFileManager alloc] init] fileExistsAtPath:dataPath])
-        data = [NSKeyedUnarchiver unarchiveObjectWithFile:dataPath];
-//    if([data count] == 0) {
-//        [data insertObject:@"61" atIndex:0];
-//        [data insertObject:@"Current Location, 00000" atIndex:1];
-//        [[NSKeyedArchiver archivedDataWithRootObject:data] writeToFile:dataPath atomically:YES];
-//    }
-    _items = data;
-    NSLog(@"%@", _items);
     [alarms reloadData];
 }
 
-- (IBAction)addalarm:(id)sender
-{
-    // Here we need to pass a full frame
+- (IBAction)addalarm:(id)sender {
     CustomIOS7AlertView *alertView = [[CustomIOS7AlertView alloc] init];
-    
-    // Add some custom content to the alert view
-    [alertView setContainerView:[self createDemoView]];
-    
-    // Modify the parameters
+    [alertView setContainerView:[self createTimePicker]];
     [alertView setButtonTitles:[NSMutableArray arrayWithObjects:@"Cancel", @"Add Alarm", nil]];
     [alertView setDelegate:self];
-    
-    // You may use a Block, rather than a delegate.
     [alertView setOnButtonTouchUpInside:^(CustomIOS7AlertView *alertView, int buttonIndex) {
-        NSLog(@"Block: Button at position %d is clicked on alertView %d.", buttonIndex, (int)[alertView tag]);
         [alertView close];
     }];
-    
     [alertView setUseMotionEffects:false];
-    
-    // And launch the dialog
     [alertView show];
-
 }
 
-- (void)customIOS7dialogButtonTouchUpInside: (CustomIOS7AlertView *)alertView clickedButtonAtIndex: (NSInteger)buttonIndex
-{
-    NSLog(@"Delegate: Button at position %d is clicked on alertView %d.", (int)buttonIndex, (int)[alertView tag]);
+- (void)customIOS7dialogButtonTouchUpInside: (CustomIOS7AlertView *)alertView clickedButtonAtIndex: (NSInteger)buttonIndex {
     if(buttonIndex == 1) {
         NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
-        [outputFormatter setDateFormat:@"h:mm a"]; //24hr time format
+        [outputFormatter setDateFormat:@"h:mm a"];
         NSString *dateString = [outputFormatter stringFromDate:datePicker.date];
-
         NSArray *alarm = @[@"Alarm Name1", dateString, @"1"];
+        
+        //Saving New Alarm
         NSString* dataPath = [[NSString alloc] initWithString:[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"data.plist"]];
         NSMutableArray *data = [[NSMutableArray alloc] init];
         if([[[NSFileManager alloc] init] fileExistsAtPath:dataPath])
@@ -189,54 +125,45 @@ UIDatePicker *datePicker;
     }
 }
 
-- (UIView *)createDemoView
-{
+#pragma mark - Database Functions
+
+- (void) loadOldAlarms {
+    NSString* dataPath = [[NSString alloc] initWithString:[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"data.plist"]];
+    NSMutableArray *data = [[NSMutableArray alloc] init];
+    if([[[NSFileManager alloc] init] fileExistsAtPath:dataPath]) data = [NSKeyedUnarchiver unarchiveObjectWithFile:dataPath];
+    _items = data;
+    [alarms reloadData];
+}
+
+#pragma mark - Interface Functions
+
+- (void)viewDidLoad {
+    alarms = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width, [[UIScreen mainScreen] bounds].size.height)];
+    alarms.delegate = self;
+    alarms.dataSource = self;
+    [alarms setBackgroundColor:[UIColor blackColor]];
+    [self.view addSubview:alarms];
+    [self setNeedsStatusBarAppearanceUpdate];
+    UIBarButtonItem *addAlert = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addalarm:)];
+    [self.navigationItem setRightBarButtonItem:addAlert];
+    [self.navigationItem setTitle:@"CodeOski"];
+    [self loadOldAlarms];
+    [super viewDidLoad];
+}
+
+- (UIView *)createTimePicker {
     UIView *demoView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 290, 250)];
-    
     UILabel *header = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, 290, 20)];
     [header setFont:[UIFont fontWithName:@"Roboto-Regular" size:25]];
     [header setTextAlignment:NSTextAlignmentCenter];
     [header setText:@"Set Alarm Time"];
     [demoView addSubview:header];
-    
-    
-
     datePicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, 40, 290, 200)];
     datePicker.datePickerMode = UIDatePickerModeTime;
     datePicker.hidden = NO;
     datePicker.date = [NSDate date];
-   // datePicker.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
-    
     [demoView addSubview:datePicker];
-    
     return demoView;
-}
-
-
-
-
-
-- (void) wakeup: (NSTimer *) theTimer{
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Alarm"
-                                                    message:@"Wake up or pay up"
-                                                   delegate:self
-                                          cancelButtonTitle:nil
-                                          otherButtonTitles:@"Snooze"];
-    //[alert show]; need to call when NSTimer Runs out
-}
-
-
-- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
-    // the user clicked OK
-    if (buttonIndex == 0) {
-        //reset alarm and call paypal api
-    }
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (UIStatusBarStyle) preferredStatusBarStyle {
